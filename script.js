c67331ee9d42ac8ecd7cf307a7fd49cfc70dc61b
mannatgupta146 <mannatgupta146@gmail.com>
2025-11-05 16:03:44 +0530

    completed

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Pure Beginner Practice
+// Level 1 - Pure Beginner Practice
 
 // 1. Print number from 1 to 10
 for(let i = 1; i <= 10; i++){
@@ -43,3 +43,46 @@ else{
 }
 
 // 7. Ask user’s age and check if eligible to vote If age >= 18 → “Eligible”, else → “Not eligible”
+let age = prompt("enter your age")
+
+if(age.trim() === "" || age === null){
+    console.error("enter something")
+}
+else if(isNaN(Number(age))){
+    console.error("enter a number")
+}
+else if(Number(age) >= 18){
+    console.log("eligible")
+}
+else{
+    console.log("not eligible")
+}
+
+// 8. Print multiplication table of 5
+for(let i = 1; i <= 10; i++){
+    console.log(`5 * ${i} = ${5*i}`)
+}
+
+// 9. Count how many numbers between 1 and 15 are greater than 8
+let count  = 0;
+for(let i = 1;i <= 15;i++){
+    if(i>8){
+        count++;
+        console.log(`${count}. ${i} is greater than 8`)
+    } 
+}
+console.log(`Total numbers greater than 8: ${count}`);
+
+// 10. Ask user for password and print access status
+let password = "hello123"
+let pass = prompt("enter your password")
+
+if(pass === null || pass.trim() === ""){
+    console.error("enter password")
+}
+else if(pass === password){
+    console.log("given access")    
+}
+else{
+    console.log("access denied") 
+}
